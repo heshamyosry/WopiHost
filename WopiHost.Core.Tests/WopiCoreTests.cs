b13e3a6c31f4c280b8b1af330670106b6f4ff5f4
Ludmila Kellerová <dvojitywendy@seznam.cz>
2021-11-01 15:49:15 +0100

    #133 Creation of Core Test ToUnixTimestamp

---
+++ WopiHost.Core.Tests/WopiCoreTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using WopiHost.Core;
+
+namespace WopiHost.Core.Tests
+{
+    public class WopiCoreTests
+    {
+
+        public WopiCoreTests()
+        {
+        }
+
+        public void ToUnixTimestampTest()
+        {
+            // Arrange
+            long ticks = 1635717257;
+            DateTime dateTime = new(ticks);
+
+            // Act
+
+            long actual = Extensions.ToUnixTimestamp(dateTime.Date);
+
+            // Assert
+            Assert.Equal(ticks, actual);
+        }
+
+    }
+}
